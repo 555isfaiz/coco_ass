eff3350e699c67fc14b857448d0371768c0b3bf5
555isfaiz <1244274837@qq.com>
2022-01-20 11:02:26 +0000

    finish ADCE(it can pass all given tests now)

--- llvm-passes/Adce.cpp
+++ llvm-passes/Adce.cpp
@@ -10,7 +10,19 @@ namespace
         static char ID;
         Adce() : FunctionPass(ID) {}
         virtual bool runOnFunction(Function &F) override;
+    private:
+        bool contains(SmallVector<Instruction*, 32> list, Instruction *I);
     };
+}
+
+bool Adce::contains(SmallVector<Instruction*, 32> list, Instruction *I)
+{
+    for (auto i : list)
+    {
+        if (i == I)
+            return true;
+    }
+    return false;
 }
 
 bool Adce::runOnFunction(Function &F)
@@ -20,15 +32,32 @@ bool Adce::runOnFunction(Function &F)
 
     SmallVector<Instruction*, 32> worklist;
     SmallVector<Instruction*, 32> used;
+    SmallVector<Instruction*, 32> unused;
     bool changed = false;
     auto iter = instructions(F).end();
     bool last_inst = true;
+
+    // determin used instructions by UD chain
     do
     {
         iter--;
         Instruction *I = &*iter;
         LOG_LINE("instruction: " << *I);
-        if (last_inst)
+        bool should_visit = last_inst;
+
+        if (isa<CallInst>(I))
+            should_visit = true;
+
+        if (isa<StoreInst>(I))
+            should_visit = true;
+
+        if (LoadInst *L = dyn_cast<LoadInst>(I))
+        {
+            if (L->isVolatile())
+                should_visit = true;
+        }
+
+        if (should_visit)
         {
             last_inst = false;
             worklist.push_back(I);
@@ -41,60 +70,36 @@ bool Adce::runOnFunction(Function &F)
                     if (!isa<Instruction>(U.get()))
                         continue;
                         
+                    if (contains(used, cast<Instruction>(U)))
+                        continue;
+
                     LOG_LINE("instruction: " << I << " uses: " << *U);
                     worklist.push_back(cast<Instruction>(U));
                     used.push_back(cast<Instruction>(U));
                 }
             }
         }
-        // else
-        // {
-
-        // }
 
     } while (iter != instructions(F).begin());
 
+    // pick out the unused instructions
     while (iter != instructions(F).end())
     {
-        LOG_LINE("walking: " << *iter);
         Instruction *I = &*iter;
-        LOG_LINE("walking: " << I);
-        bool Iused = false;
-        for (auto i : used)
+
+        if (!contains(used, I))
         {
-            if (i == I)
-                Iused = true;
+            LOG_LINE("removing: " << *I);
+            unused.push_back(I);
         }
-
-        if (!Iused)
-        {
-            bool can_remove = true;
-            if (LoadInst *L = dyn_cast<LoadInst>(I))
-            {
-                if (L->isVolatile())
-                    can_remove = false;
-            }
-
-            if (StoreInst *S = dyn_cast<StoreInst>(I))
-            {
-                if (S->isVolatile())
-                    can_remove = false;
-            }
-            
-            if (!I->isSafeToRemove())
-                can_remove = false;
-
-            if (can_remove)
-            {
-                changed = true;
-                LOG_LINE("removing: " << I);
-                LOG_LINE("removing: " << *I);
-                I->eraseFromParent();
-            }
-        }
-        LOG_LINE("iter then: " << *iter);
         iter++;
-        LOG_LINE("iter now: " << *iter);
+    }
+
+    // remove unused
+    for (auto *I : unused)
+    {
+        changed = true;
+        I->eraseFromParent();
     }
     
     return changed;
